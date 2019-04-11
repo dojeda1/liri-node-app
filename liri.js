fd8d307480685235269be2836cda9ffa5a3471e4
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-04-11 11:31:12 -0600

    Takes process.argv[2] and runs a cresponding command.

--- liri.js
+++ liri.js
@@ -1,3 +1,28 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var spotify = keys.spotify;
+
+// console.log(spotify.id);
+// console.log(spotify.secret);
+
+var command = process.argv[2];
+console.log(command);
+
+switch (command) {
+    case "concert-this":
+        console.log("1");
+        break;
+
+    case "spotify-this-song":
+        console.log("2");
+        break;
+
+    case "movie-this":
+        console.log("3");
+        break;
+
+    case "do-what-it-says":
+        console.log("4");
+        break;
+
+}
